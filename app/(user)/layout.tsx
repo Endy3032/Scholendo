595a79c2a6bbd76c7dea90566e6a3922d4f7428f
Endy <endy@endy3032.ml>
2023-10-31 18:39:35 +0000

    space heading

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body className="h-screen">
-				<main className="flex flex-col h-screen justify-center items-center">
+				<main className="flex flex-col space-y-4 h-screen justify-center items-center">
 					{children}
 				</main>
 			</body>
