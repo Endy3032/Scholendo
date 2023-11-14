c7114c7f09b9831c5466d98e380424d49290f747
Endy <endy@endy3032.ml>
2023-11-14 15:44:13 +0000

    fix

--- app/app/timetable/page.tsx
+++ app/app/timetable/page.tsx
@@ -1,7 +1,7 @@
 const Timetable = async () => {
 	return (
 		<>
-			<h1 className="font-semibold text-4xl">Homework</h1>
+			<h1 className="font-semibold text-4xl">Timetable</h1>
 			<p>In development!</p>
 		</>
 	)
