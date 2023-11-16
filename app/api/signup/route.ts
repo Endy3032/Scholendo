6834081768ee65e10789b1db7ff0dbae3b6625a9
Endy3032 <endy3032@icloud.com>
2023-11-17 00:44:35 +0700

    testxi

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
+import getPayloadClient from "payload/client"
 import { PaginatedDocs } from "payload/database"
 import { Options } from "payload/dist/collections/operations/local/create"
 import { User } from "payload/generated-types"
 import qs from "qs"
 
 export async function POST(request: NextRequest) {
+	const payload = await getPayloadClient()
+	console.log(payload)
 	const { name, email, password } = await request.json() as Options<"users">["data"]
 
 	console.log(name, email, password)
