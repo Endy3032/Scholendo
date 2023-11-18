f7900cce7f3083604b2d8355229aed52d42c751c
Endy3032 <endy3032@icloud.com>
2023-11-18 21:05:34 +0700

    moretest

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,6 +1,6 @@
+import axios from "axios"
 import { payloadApiUrl } from "lib/const"
 import { NextRequest, NextResponse } from "next/server"
-import getPayloadClient from "payload/client"
 import { PaginatedDocs } from "payload/database"
 import { Options } from "payload/dist/collections/operations/local/create"
 import { User } from "payload/generated-types"
@@ -9,10 +9,11 @@ import qs from "qs"
 export async function POST(request: NextRequest) {
 	const { name, email, password } = await request.json() as Options<"users">["data"]
 
-	const payload = await getPayloadClient()
-	console.log(payload)
-
 	const searchQuery = qs.stringify({ where: { email: { equals: email } } })
+
+	const test = await axios.get(`${payloadApiUrl}/users?${searchQuery}`)
+	console.log(test.data)
+
 	const searchResult = await fetch(`${payloadApiUrl}/users?${searchQuery}`)
 	const data = await searchResult.text()
 	console.log(data)
