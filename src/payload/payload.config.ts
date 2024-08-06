379fb60636393234cac8622f22425dfedc92678f
Endy3032 <endy3032@icloud.com>
2024-08-07 01:42:18 +0700

    update payload

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -41,13 +41,13 @@ export default buildConfig({
 	email: nodemailerAdapter({
 		defaultFromAddress: process.env.SMTP_FROM_ADDRESS ?? "",
 		defaultFromName: process.env.SMTP_FROM_NAME ?? "",
-		transport: nodemailer.createTransport({
+		transportOptions: {
 			host: process.env.SMTP_HOST ?? "",
 			port: parseInt(process.env.SMTP_PORT ?? "587"),
 			auth: {
 				user: process.env.SMTP_USER ?? "",
 				pass: process.env.SMTP_PASS ?? "",
 			},
-		}),
+		},
 	}),
 })
